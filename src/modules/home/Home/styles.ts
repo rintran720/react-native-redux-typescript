5c0d0e10f45092a3fa84de3c55975662a95acfbf
John Smith <rintran720@gmail.com>
2022-12-12 09:54:12 +0700

    feat: migrate useAPI from react repo to RN #1

--- src/modules/home/Home/styles.ts
+++ src/modules/home/Home/styles.ts
@@ -2,18 +2,18 @@ import { ThemeOptions } from '@rneui/themed';
 import { StyleSheet } from 'react-native';
 
 const styles = ({ colors, fontSizes, spacing }: ThemeOptions) =>
-	StyleSheet.create({
-		container: {
-			flex: 1,
-			backgroundColor: colors.background,
-			padding: spacing.lg,
-		},
-		applyFont: {
-			// fontFamily: fontFamilies.body,
-			fontStyle: 'italic',
-			fontWeight: '700',
-			fontSize: fontSizes.xl,
-		},
-	});
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      padding: spacing.lg,
+    },
+    applyFont: {
+      // fontFamily: fontFamilies.body,
+      fontStyle: 'italic',
+      fontWeight: '700',
+      fontSize: fontSizes.xl,
+    },
+  });
 
 export default styles;
--- src/modules/profile/Profile/styles.ts
+++ src/modules/profile/Profile/styles.ts
@@ -2,11 +2,11 @@ import { ThemeOptions } from '@rneui/themed';
 import { StyleSheet } from 'react-native';
 
 const styles = (theme: ThemeOptions) =>
-	StyleSheet.create({
-		container: {
-			flex: 1,
-			backgroundColor: theme.colors.background,
-		},
-	});
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+  });
 
 export default styles;
